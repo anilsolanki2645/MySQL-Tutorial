7e5d4af0e2f3cfbeeb441bb0654843582feff959
Anil <anilsolanki1834@gmail.com>
2024-05-29 22:01:00 +0530

    Store Procedure using mysql

---
+++ 5_Store_Procedures.sql
@@ -0,0 +1,19 @@
+-- -----------------------------------------------------------------------------------------
+                                    -- 5. MySQL - Store Procedures
+-- -----------------------------------------------------------------------------------------
+
+-- [1] What are Store Procedures in MySQL?
+/*      MySQL stored procedures group multiple tasks into one and save the task on the server for future use.
+        Stored procedures simplify database management and reduce network traffic.
+        MySQL stored procedures are pre-compiled SQL statements stored in a database. They are subroutines containing a name, a parameter list, and SQL statements.
+        They are similar to functions or methods in programming languages.
+*/
+
+
+
+
+
+
+
+
+
