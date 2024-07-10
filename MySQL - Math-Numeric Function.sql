6413fa12fbc55ec655607b98266ba3f54467b9cd
Anil <anilsolanki1834@gmail.com>
2024-07-10 21:27:30 +0530

    Update file math-numeric function

--- MySQL - Math-Numeric Function.sql
+++ MySQL - Math-Numeric Function.sql
@@ -447,3 +447,16 @@
     -- Returns the numeric expression rounded to an integer. Can be used to round an expression to a number of decimal points.
     SELECT ROUND(123.456);     -- Returns 123
     SELECT ROUND(123.456, 2);  -- Returns 123.46
+
+/*
++----------------+
+| ROUND(123.456) |
++----------------+
+|            123 |
++----------------+
+
++-------------------+
+| ROUND(123.456, 2) |
++-------------------+
+|            123.46 |
++-------------------+   */
