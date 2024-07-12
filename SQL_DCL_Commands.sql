dd63e5b689197f2582d19067534cfc5978b5f1ea
Anil <anilsolanki1834@gmail.com>
2024-07-12 21:53:04 +0530

    Create DCL(DATA CONTROL LANGUAGE) COMMANDS File

---
+++ SQL_DCL_Commands.sql
@@ -0,0 +1,9 @@
+-- -------------------------------------------------------------------------------
+					-- DCL(DATA CONTROL LANGUAGE) COMMANDS
+-- -------------------------------------------------------------------------------
+
+-- -------------------------------- [1] GRANT COMMAND -------------------------------------
+
+-- 1.1 Grant All Privileges to a User on a Databas
+GRANT ALL PRIVILEGES ON database_name.* TO 'user'@'hostname';
+
