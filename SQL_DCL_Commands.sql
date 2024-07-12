6c8093a3199f8116d5eddbdb13e0b74cee0f5a0d
Anil <anilsolanki1834@gmail.com>
2024-07-12 21:53:34 +0530

    Update DCL(DATA CONTROL LANGUAGE) COMMANDS File

--- SQL_DCL_Commands.sql
+++ SQL_DCL_Commands.sql
@@ -7,3 +7,5 @@
 -- 1.1 Grant All Privileges to a User on a Databas
 GRANT ALL PRIVILEGES ON database_name.* TO 'user'@'hostname';
 
+-- 1.2 Grant Specific Privileges to a User on a Database
+GRANT SELECT, INSERT, UPDATE ON database_name.* TO 'user'@'hostname';
