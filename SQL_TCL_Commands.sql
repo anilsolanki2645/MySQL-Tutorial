07896e3808d0d98808d1241135fcc5153de5f48e
Anil <anilsolanki1834@gmail.com>
2024-07-24 20:54:48 +0530

    UPDATE TCL(TRANSACTION CONTROL LANGUAGE) COMMANDS File

--- SQL_TCL_Commands.sql
+++ SQL_TCL_Commands.sql
@@ -7,3 +7,9 @@
 -- COMMIT: This command is used to permanently save the changes made during the current transaction
 
 COMMIT;
+
+-- -------------------------------- [2] ROLLBACK COMMAND -------------------------------------
+
+-- ROLLBACK: This command is used to undo all changes made during the current transaction.Example:
+
+ROLLBACK;
