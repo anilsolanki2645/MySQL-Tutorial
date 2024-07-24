d3d930f2138b5eff977d0b2d82abf9cd25916088
Anil <anilsolanki1834@gmail.com>
2024-07-24 20:53:42 +0530

    CREATE TCL(TRANSACTION CONTROL LANGUAGE) COMMANDS File

---
+++ SQL_TCL_Commands.sql
@@ -0,0 +1,9 @@
+-- -------------------------------------------------------------------------------
+					-- TCL(TRANSACTION CONTROL LANGUAGE) COMMANDS
+-- -------------------------------------------------------------------------------
+
+-- -------------------------------- [1] COMMIT COMMAND -------------------------------------
+
+-- COMMIT: This command is used to permanently save the changes made during the current transaction
+
+COMMIT;
