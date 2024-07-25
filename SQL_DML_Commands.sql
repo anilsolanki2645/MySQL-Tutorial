d9817047ebf633c84b753fcb5ce7a87cac456863
Anil <anilsolanki1834@gmail.com>
2024-07-25 21:17:24 +0530

    Update DML (Data Manipulation Language) COMMANDS File

--- SQL_DML_Commands.sql
+++ SQL_DML_Commands.sql
@@ -9,3 +9,6 @@ SELECT * FROM employees;
 
 -- 1.2 INSERT: Adds new data into a database table.
 INSERT INTO employees (name, position, salary) VALUES ('John Doe', 'Manager', 60000);
+
+-- 1.3 UPDATE: Modifies existing data in a database table.
+UPDATE employees SET salary = 65000 WHERE name = 'John Doe';
