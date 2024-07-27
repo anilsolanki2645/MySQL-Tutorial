0c6c0dae92d42705c3603c17b8512e90fccf87b3
Anil <anilsolanki1834@gmail.com>
2024-07-27 22:07:56 +0530

    Update DML (Data Manipulation Language) COMMANDS File

--- SQL_DML_Commands.sql
+++ SQL_DML_Commands.sql
@@ -16,4 +16,12 @@ UPDATE employees SET salary = 65000 WHERE name = 'John Doe';
 -- 1.4 DELETE: Removes data from a database table.
 DELETE FROM employees WHERE name = 'John Doe';
 
+-- 2 MERGE (also known as UPSERT in some databases): Combines INSERT and UPDATE operations, but MySQL doesn’t have a direct MERGE command. Instead, you can use INSERT ... ON DUPLICATE KEY UPDATE.
+INSERT INTO employees (id, name, position, salary)
+VALUES (1, 'John Doe', 'Manager', 60000)
+ON DUPLICATE KEY UPDATE
+name = VALUES(name),
+position = VALUES(position),
+salary = VALUES(salary);
+
 
