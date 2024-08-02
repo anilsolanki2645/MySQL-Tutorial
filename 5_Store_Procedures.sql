d65f76efbdd34341a0ad0390653a0caf93a585ab
Anil <anilsolanki1834@gmail.com>
2024-08-02 21:06:32 +0530

    Update Store Procedure file

--- 5_Store_Procedures.sql
+++ 5_Store_Procedures.sql
@@ -212,13 +212,6 @@
 
 -- 10.3 Stored Procedure with UPDATE:
 
-        DELIMITER //
-
-        DROP PROCEDURE IF EXISTS UpdateDemoRecord;
-        CREATE PROCEDURE UpdateDemoRecord(IN record_id INT, IN new_name VARCHAR(100))
-        BEGIN
-            UPDATE TEMP_DB.DEMO2 SET name = new_name WHERE id = record_id;
-        END //
         
         DELIMITER ;
         
