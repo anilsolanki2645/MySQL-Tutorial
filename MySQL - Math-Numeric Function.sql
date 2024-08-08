d2af31275aef6b0e85f06bb7e4cd6bc676280d2f
Anil <anilsolanki1834@gmail.com>
2024-08-08 20:48:01 +0530

    Update Math Function file

--- MySQL - Math-Numeric Function.sql
+++ MySQL - Math-Numeric Function.sql
@@ -22,13 +22,7 @@
 |       8 |
 +---------+ */
 
--- [2] ACOS()
 
-    -- Returns the arccosine of a numeric expression. Returns NULL if the value is not in the range -1 to 1.
-    SELECT ACOS(1);  -- Returns 0 (0 radians, which is 0 degrees)
-    SELECT ACOS(0);  -- Returns 1.5708 (π/2 radians, which is 90 degrees)
-
-/*
 +---------+
 | ACOS(1) |
 +---------+
