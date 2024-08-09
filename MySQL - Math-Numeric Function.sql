4efdc0d8f3152933459131479e5595bcd40b16bb
Anil <anilsolanki1834@gmail.com>
2024-08-09 22:50:37 +0530

    Update Math Function file

--- MySQL - Math-Numeric Function.sql
+++ MySQL - Math-Numeric Function.sql
@@ -228,7 +228,9 @@
 
 -- [13] FLOOR()
 
-    
+    -- Returns the largest integer value that is not greater than the passed numeric expression.
+    SELECT FLOOR(2.7);  -- Returns 2
+    SELECT FLOOR(-2.7); -- Returns -3
 /*
 +------------+
 | FLOOR(2.7) |
