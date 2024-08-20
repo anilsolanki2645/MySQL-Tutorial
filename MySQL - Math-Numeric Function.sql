bf5a9a221cb3eca403316c6593495efbd9a1a4d6
Anil <anilsolanki1834@gmail.com>
2024-08-20 21:27:50 +0530

    Update Math Function file

--- MySQL - Math-Numeric Function.sql
+++ MySQL - Math-Numeric Function.sql
@@ -320,24 +320,7 @@
 |           3 |
 +-------------+     */
 
--- [18] LOG2()
 
-    -- Returns the base-2 logarithm of the passed numeric expression.
-    SELECT LOG2(8);  -- Returns 3
-    SELECT LOG2(16); -- Returns 4
-
-/*
-+---------+
-| LOG2(8) |
-+---------+
-|       3 |
-+---------+
-
-+----------+
-| LOG2(16) |
-+----------+
-|        4 |
-+----------+    */
 
 -- [19] MOD()
 
